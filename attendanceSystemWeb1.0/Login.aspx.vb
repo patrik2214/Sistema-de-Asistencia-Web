d191e96fa333f1dcd57550c5887855261e8f91ec
Sara Benel <sbenelramirez@gmail.com>
2020-06-23 16:00:01 -0500

    cambio de ensamblados con bd nueva

--- attendanceSystemWeb1.0/Login.aspx.vb
+++ attendanceSystemWeb1.0/Login.aspx.vb
@@ -1,26 +1,24 @@
 ﻿Imports capaNegocio
 Imports capaDatos
 
-
 Public Class WebForm1
     Inherits System.Web.UI.Page
     Dim usuario As New clsUsuario
-    Private users As capaDatos.Users
+    Private users As capaDatos.users
     Protected Sub Btn_Login_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnLogin.Click
         Try
             User = New clsUsuario().Login(txtNombreUsuario.Text, txtContraseña.Text)
-            Dim username = User.Employee.Name + " " + User.Employee.Lastname
             Response.Redirect("Index.aspx")
         Catch ex As Exception
 
         End Try
     End Sub
 
-    Public Property User() As capaDatos.Users
+    Public Property User() As capaDatos.users
         Get
             Return users
         End Get
-        Set(value As capaDatos.Users)
+        Set(value As capaDatos.users)
             users = value
         End Set
     End Property
